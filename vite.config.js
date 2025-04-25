146fb8eb226caa1a96ec91333ce5015ec982776f
Ramanand Mandal <mrazzanand@gmail.com>
2025-04-25 23:57:53 +0545

    changes

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,21 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled']
+        }
+      }
+    }
+  },
+  preview: {
+    port: 3000
   }
 }) 
